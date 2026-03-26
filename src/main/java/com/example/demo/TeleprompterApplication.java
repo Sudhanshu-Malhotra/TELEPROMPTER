e2dfc01d3ba00ea9d3c9b2ea7293ce5c690969be
Sudhanshu Malhotra <malhotrasudhanshu128@gmail.com>
2026-03-26 21:22:40 +0530

    Initial commit: Spring boot project setup

---
+++ src/main/java/com/example/demo/TeleprompterApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleprompterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeleprompterApplication.class, args);
+	}
+
+}
